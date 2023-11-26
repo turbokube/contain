af65f14820fe9285a58741bceed6b29fe04c793f
Staffan Olsson <staffan@repos.se>
2023-11-26 14:22:31 +0100

    Next step might be to help define tests that run fast, maybe using
    Dagger which seems to offer explicit control over arch choices.
    Also it offers quite simple access to a container file system,
    so we don't get stuck with testing the individual layers.
    For example I verified that the arm64 and amd64 files respectively
    exist in
    
    	test2 := client.Container().From("solsson/multiarch-test:noattest")
    	entries2, err := test2.Rootfs().Directory("/").Entries(ctx)
    	if err != nil {
    		return err
    	}
    	fmt.Println(entries2)
    
    	test3 := client.Container(dagger.ContainerOpts{
    		Platform: "linux/amd64",
    	}).From("solsson/multiarch-test:noattest")
    	entries3, err := test3.Rootfs().Directory("/").Entries(ctx)
    	if err != nil {
    		return err
    	}
    	fmt.Println(entries3)

---
+++ pkg/contain/libcontain.go
@@ -0,0 +1,12 @@
+package contain
+
+import (
+	schema "github.com/turbokube/contain/pkg/schema/v1"
+)
+
+// Run uses the provided config for a complete contain run
+// and returns the resulting image with digest
+func Run(config *schema.ContainConfig) (string, error) {
+	// TODO see cmd main.go
+	return "", nil
+}
