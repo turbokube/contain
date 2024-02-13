bdff23157fa8f7cbdaf935367faeee763f6e2d20
Staffan Olsson <staffan@repos.se>
2024-02-13 10:41:22 +0100

    begin append, todo construct new index

--- pkg/multiarch/imageindex.go
+++ pkg/multiarch/imageindex.go
@@ -1,6 +1,7 @@
 package multiarch
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/google/go-containerregistry/pkg/name"
@@ -12,9 +13,16 @@ import (
 	"go.uber.org/zap"
 )
 
+// imageIndexItem is sufficient to produce a result manifest at the end
+type imageIndexItem struct {
+	base name.Digest
+}
+
 type ImageIndex struct {
-	baseRef      name.Digest
-	prototypeRef *name.Digest
+	baseRef   name.Digest
+	prototype *imageIndexItem
+	remaining []*imageIndexItem
+	append    *appender.AppendResult
 }
 
 func NewFromMultiArchBase(config schema.ContainConfig, baseRegistry *registry.RegistryConfig) (*ImageIndex, error) {
@@ -48,40 +56,80 @@ func NewFromMultiArchBase(config schema.ContainConfig, baseRegistry *registry.Re
 		return nil, fmt.Errorf("image index manifest from %s %s", base.MediaType, config.Base)
 	}
 
+	index := &ImageIndex{
+		baseRef:   baseRef,
+		remaining: make([]*imageIndexItem, 0),
+	}
+
 	requireMediaType := types.OCIManifestSchema1
-	var prototypeRef *name.Digest
-	for _, m := range baseIndexManifest.Manifests {
-		if m.MediaType == requireMediaType {
-			p := baseRef.Digest(m.Digest.String())
-			prototypeRef = &p
-			break
+	for i, m := range baseIndexManifest.Manifests {
+		zap.L().Debug("index",
+			zap.Int("item", i),
+			zap.String("mediaType", string(m.MediaType)),
+			zap.String("platform", m.Platform.String()),
+		)
+		if m.Platform == nil {
+			zap.L().Info("skipping layer without platform",
+				zap.String("got", string(m.MediaType)),
+				zap.String("supported", string(m.MediaType)),
+			)
+			continue
+		}
+		if m.MediaType != requireMediaType {
+			zap.L().Warn("skipping unsupported media type",
+				zap.String("got", string(m.MediaType)),
+				zap.String("supported", string(m.MediaType)),
+			)
+			continue
+		}
+		item := &imageIndexItem{
+			base: index.baseRef.Digest(m.Digest.String()),
+		}
+		if index.prototype == nil {
+			index.prototype = item
+		} else {
+			index.remaining = append(index.remaining, item)
 		}
 	}
-	if prototypeRef == nil {
+
+	if index.prototype == nil {
 		raw, err := baseIndex.RawManifest()
 		if err != nil {
 			return nil, fmt.Errorf("raw manifest for debugging %v", err)
 		}
-		return nil, fmt.Errorf("found no manifest of type %s in index %s %v", requireMediaType, baseRef, raw)
+		return nil, fmt.Errorf("found no platform manifest of type %s in index %s %v", requireMediaType, baseRef, raw)
+	}
+	// reminder: we're stricter than necessary in early iterations, to help standardize on index types
+	if len(index.remaining) == 0 {
+		raw, err := baseIndex.RawManifest()
+		if err != nil {
+			return nil, fmt.Errorf("raw manifest for debugging %v", err)
+		}
+		return nil, fmt.Errorf("found only one platform manifest of type %s in index %s %v", requireMediaType, baseRef, raw)
 	}
 
-	return &ImageIndex{
-		baseRef:      baseRef,
-		prototypeRef: prototypeRef,
-	}, nil
+	return index, nil
 }
 
-func (m *ImageIndex) GetPrototype() (name.Digest, error) {
-	return *m.prototypeRef, nil
+func (m *ImageIndex) GetPrototypeBase() (name.Digest, error) {
+	return m.prototype.base, nil
 }
 
 // WithPrototypeAppend should be called when appender has run on the prototype image
 func (m *ImageIndex) WithPrototypeAppend(result appender.AppendResult) error {
-	panic("TODO")
+	m.append = &result
+	// TODO produce result manifests here, to validate result and bases
+	// probably in the item impls
+	// maybe we don't need the .append prop
+	// remember platform
+	return nil
 }
 
 // PushIndex derives indexes from prototype + append and pushes them
 // with the assumption that append has pushed all the layers
 func (m *ImageIndex) PushIndex(registry *registry.RegistryConfig) error {
+	if m.append == nil {
+		return errors.New("WithPrototypeAppend has not been called")
+	}
 	return nil
 }
--- pkg/multiarch/imageindex_test.go
+++ pkg/multiarch/imageindex_test.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"testing"
 
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/types"
 	. "github.com/onsi/gomega"
+	"github.com/turbokube/contain/pkg/appender"
 	"github.com/turbokube/contain/pkg/multiarch"
 	schema "github.com/turbokube/contain/pkg/schema/v1"
 	"github.com/turbokube/contain/pkg/testcases"
@@ -34,12 +37,33 @@ func TestImageIndex(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	prototype, err := index.GetPrototype()
+	prototype, err := index.GetPrototypeBase()
 	if err != nil {
 		t.Error(err)
 	}
-	// the first item in the manifests array happens to be amd64
+	// we don't care which arch that is prototype, but the first item in the manifests array happens to be amd64
 	Expect(prototype.DigestStr()).To(Equal("sha256:88b8e36da2fe3947b813bd52473319c3fb2e7637692ff4c499fa8bd878241852"))
 	Expect(prototype.String()).To(Equal(fmt.Sprintf("%s/contain-test/baseimage-multiarch1@sha256:88b8e36da2fe3947b813bd52473319c3fb2e7637692ff4c499fa8bd878241852", r.Host)))
+
+	err = index.PushIndex(&r.Config)
+	Expect(err).To(MatchError("WithPrototypeAppend has not been called"))
+
+	// mock result from appender package
+	manifesthash, err := v1.NewHash("sha256:50c393f158c3de2db92fa9661bfb00eda5b67c3a777c88524ed3417509631625")
+	Expect(err).NotTo(HaveOccurred())
+	layerhash, err := v1.NewHash("sha256:4355a46b19d348dc2f57c046f8ef63d4538ebb936000f3c9ee954a27460dd865")
+	Expect(err).NotTo(HaveOccurred())
+	result := appender.AppendResult{
+		Hash: manifesthash,
+		AddedManifestLayers: []appender.AppendResultLayer{
+			{
+				MediaType: types.OCILayer,
+				Size:      1,
+				Digest:    layerhash,
+			},
+		},
+	}
+	err = index.WithPrototypeAppend(result)
+	Expect(err).NotTo(HaveOccurred())
 
 }
