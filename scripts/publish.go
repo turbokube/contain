135ac0b544a281b6d6bfc7117763bbded0bf4a3e
Staffan Olsson <staffan@repos.se>
2024-04-01 20:04:11 +0200

    0.4.1

--- scripts/publish.go
+++ scripts/publish.go
@@ -24,7 +24,7 @@ const (
 )
 
 var (
-	publishVersion    = "0.4.0"
+	publishVersion    = "0.4.1"
 	releaseBinaryName = regexp.MustCompile(`^contain-v(?P<version>\d+\.\d+\.\d+)-(?P<os>[a-z0-9]+)-(?P<arch>[a-z0-9]+)(?P<ext>\.exe)?(?P<checksum>\.[a-z0-9]+)?$`)
 )
 
