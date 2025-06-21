c8aa965d61697661259b41a2d2ab076522e08e05
Staffan Olsson <staffan@repos.se>
2025-06-21 15:49:19 +0200

    impl by claude sonnet 4

--- pkg/contain/build_output_test.go
+++ pkg/contain/build_output_test.go
@@ -1,6 +1,7 @@
 package contain_test
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 
@@ -19,29 +20,29 @@ func TestBuildOutput(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		if len(o.Builds) != 1 {
-			t.Errorf("%d builds", len(o.Builds))
+		if len(o.Skaffold.Builds) != 1 {
+			t.Errorf("%d builds", len(o.Skaffold.Builds))
 		}
-		if o.Builds[0].Tag != "localhost:1234/test/foo:latest@sha256:deadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33f" {
-			t.Errorf("ref %s", o.Builds[0].Tag)
+		if o.Skaffold.Builds[0].Tag != "localhost:1234/test/foo:latest@sha256:deadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33f" {
+			t.Errorf("ref %s", o.Skaffold.Builds[0].Tag)
 		}
-		if o.Builds[0].ImageName != "localhost:1234/test/foo" {
-			t.Errorf("name %s", o.Builds[0].ImageName)
+		if o.Skaffold.Builds[0].ImageName != "localhost:1234/test/foo" {
+			t.Errorf("name %s", o.Skaffold.Builds[0].ImageName)
 		}
 		f, err := os.CreateTemp("", ".json")
 		if err != nil {
 			t.Error(err)
 		}
 		defer os.Remove(f.Name())
-		o.WriteJSON(f)
-		json, err := os.ReadFile(f.Name())
+		o.WriteSkaffoldJSON(f)
+		jsonBytes, err := os.ReadFile(f.Name())
 		if err != nil {
 			t.Error(err)
 		}
-		if string(json) != "{\"builds\":[{\"imageName\":\"localhost:1234/test/foo\",\"tag\":\"localhost:1234/test/foo:latest@sha256:deadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33f\"}]}" {
-			t.Errorf("json %s", json)
+		if string(jsonBytes) != "{\"builds\":[{\"imageName\":\"localhost:1234/test/foo\",\"tag\":\"localhost:1234/test/foo:latest@sha256:deadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33f\"}]}" {
+			t.Errorf("json %s", jsonBytes)
 		}
-		http := o.Builds[0].Http()
+		http := o.Skaffold.Builds[0].Http()
 		if http.Host != "localhost:1234" {
 			t.Errorf("host %s", http.Host)
 		}
@@ -61,16 +62,16 @@ func TestBuildOutput(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		if len(o.Builds) != 1 {
-			t.Errorf("%d builds", len(o.Builds))
+		if len(o.Skaffold.Builds) != 1 {
+			t.Errorf("%d builds", len(o.Skaffold.Builds))
 		}
-		if o.Builds[0].Tag != "test/foo:a@sha256:deadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33f" {
-			t.Errorf("ref %s", o.Builds[0].Tag)
+		if o.Skaffold.Builds[0].Tag != "test/foo:a@sha256:deadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33f" {
+			t.Errorf("ref %s", o.Skaffold.Builds[0].Tag)
 		}
-		if o.Builds[0].ImageName != "test/foo" {
-			t.Errorf("name %s", o.Builds[0].ImageName)
+		if o.Skaffold.Builds[0].ImageName != "test/foo" {
+			t.Errorf("name %s", o.Skaffold.Builds[0].ImageName)
 		}
-		http := o.Builds[0].Http()
+		http := o.Skaffold.Builds[0].Http()
 		if http.Host != "index.docker.io" {
 			t.Errorf("default host %s", http.Host)
 		}
@@ -90,16 +91,16 @@ func TestBuildOutput(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		if len(o.Builds) != 1 {
-			t.Errorf("%d builds", len(o.Builds))
+		if len(o.Skaffold.Builds) != 1 {
+			t.Errorf("%d builds", len(o.Skaffold.Builds))
 		}
-		if o.Builds[0].Tag != "test/foo@sha256:deadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33f" {
-			t.Errorf("ref %s", o.Builds[0].Tag)
+		if o.Skaffold.Builds[0].Tag != "test/foo@sha256:deadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33f" {
+			t.Errorf("ref %s", o.Skaffold.Builds[0].Tag)
 		}
-		if o.Builds[0].ImageName != "test/foo" {
-			t.Errorf("name %s", o.Builds[0].ImageName)
+		if o.Skaffold.Builds[0].ImageName != "test/foo" {
+			t.Errorf("name %s", o.Skaffold.Builds[0].ImageName)
 		}
-		http := o.Builds[0].Http()
+		http := o.Skaffold.Builds[0].Http()
 		if http.Host != "index.docker.io" {
 			t.Errorf("default host %s", http.Host)
 		}
@@ -111,6 +112,44 @@ func TestBuildOutput(t *testing.T) {
 		}
 		if http.Hash != h1 {
 			t.Errorf("hash %v", http.Hash)
+		}
+	})
+
+	t.Run("buildctl metadata output", func(t *testing.T) {
+		o, err := contain.NewBuildOutput("example.net/yolean/g5y-sidecar:daa3b6df7f58f7644a4ecb129af3d6f70653127c-dirty-205649", h1)
+		if err != nil {
+			t.Error(err)
+		}
+		if o.Buildctl == nil {
+			t.Error("Buildctl metadata should not be nil")
+		}
+		if o.Buildctl.ImageName != "example.net/yolean/g5y-sidecar:daa3b6df7f58f7644a4ecb129af3d6f70653127c-dirty-205649" {
+			t.Errorf("image name %s", o.Buildctl.ImageName)
+		}
+		if o.Buildctl.ContainerImageDigest != h1.String() {
+			t.Errorf("digest %s", o.Buildctl.ContainerImageDigest)
+		}
+
+		f, err := os.CreateTemp("", ".json")
+		if err != nil {
+			t.Error(err)
+		}
+		defer os.Remove(f.Name())
+		o.WriteBuildctlJSON(f)
+		jsonBytes, err := os.ReadFile(f.Name())
+		if err != nil {
+			t.Error(err)
+		}
+		// Basic check that it's valid JSON with expected fields
+		var metadata map[string]interface{}
+		if err := json.Unmarshal(jsonBytes, &metadata); err != nil {
+			t.Errorf("invalid JSON: %v", err)
+		}
+		if metadata["containerimage.digest"] != h1.String() {
+			t.Errorf("missing or wrong containerimage.digest")
+		}
+		if metadata["image.name"] != "example.net/yolean/g5y-sidecar:daa3b6df7f58f7644a4ecb129af3d6f70653127c-dirty-205649" {
+			t.Errorf("missing or wrong image.name")
 		}
 	})
 
