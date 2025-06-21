fa05f98d6d43153d510af78db1b0c70946edaaa7
Staffan Olsson <staffan@repos.se>
2025-06-21 19:22:25 +0200

    the config change from 0.6

--- pkg/schema/v1/config.go
+++ pkg/schema/v1/config.go
@@ -45,7 +45,14 @@ type LayerAttributes struct {
 
 	// Mode bits to use on files, must be a value between 0 and 0777.
 	// YAML accepts both octal and decimal values, JSON requires decimal values for mode bits.
+	// Note that if you don't specify mode, contain will try to preserve local mode which might void reproducibility.
 	FileMode int32 `json:"mode,omitempty"`
+
+	// DirMode bits to use on directories, must be a value between 0 and 0777.
+	// If not specified, the mode value will be used for directories as well.
+	// YAML accepts both octal and decimal values, JSON requires decimal values for mode bits.
+	// Note that if you don't specify mode, contain will try to preserve local mode which might void reproducibility.
+	DirMode int32 `json:"dirMode,omitempty"`
 }
 
 // LocalFile is a single file that should be appended as-is to base
